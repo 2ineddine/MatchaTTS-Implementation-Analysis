525721bb13c1500c78ac7f8773be0e1f2503b570
Massyl A. <adjalmassyl04@gmail.com>
2026-01-07 11:50:16 +0100

    implementation v2 : create empty train.py

--- implementation/data.py
+++ implementation/data.py
@@ -1,1 +1,42 @@
 # Dataset loading and text processing
+
+import numpy as np
+import torch
+
+
+
+
+# Text to sequence
+def text_to_sequence() : 
+
+
+
+
+
+
+
+# Main Text processing
+@torch.inference_mode()
+def process_text(text, device):
+    
+    # a. Convert text to phoneme IDs
+    # 'english_cleaners2' handles number expansion and phonemization via espeak
+    sequence = text_to_sequence(text, ['english_cleaners2'])[0]
+    
+    # b. Intersperse with '0' (blank token) for the flow matching stability
+    sequence = intersperse(sequence, 0)
+    
+    # c. Convert to tensor
+    x = torch.tensor(sequence, dtype=torch.long, device=device)[None] # Add batch dim
+
+    # metadatas
+    x_lengths = torch.tensor([x.shape[-1]], dtype=torch.long, device=device)
+
+    # output
+    output = {
+        'text': text,
+        'x': x,
+        'x_lengths': x_lengths
+    }
+    
+    return output
